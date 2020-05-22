af33993367487227f0b92ed3806979f60944e4e6
Esther Kang <estherkang14@gmail.com>
2020-05-22 03:02:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,8 +1,16 @@
 # Write your code here!
 require 'pry'
 def game_hash(data) 
-  {:home => {},
-  :away => {}
+  {:home => {
+    :team_name => " ", 
+    :colors => [ ], 
+    :players => [ ]
+  },
+  :away => {
+    :team_name => " ",
+    :colors => [ ],
+    :players => [ ]
+  }
   }
   
 end 
